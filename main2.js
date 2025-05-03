8ae88e6dc232df0af32d18202dd21bb5324d8232
danuloshlykow <danilshlykow123@gmail.com>
2025-05-03 19:49:21 +0200

    collegamento il menu con file index

--- main2.js
+++ main2.js
@@ -1,5 +1,6 @@
 import PromptSync from "prompt-sync";
 
+import { addAttivita,visualat } from './index.js';
 let prompt = PromptSync();
 
 let a;
@@ -7,17 +8,23 @@ let a;
 do {
     console.log("Scegli cosa vuoi fare");
 
-    console.log(" 0=agiungi attivita, 1=cancelazione di attivita , 2=modifica di attivita" );
+    console.log(" 0=agiungi attivita, 1=cancelazione di attivita , 2=modifica di attivita,3=visualizzare le attivata" );
     a = prompt("");
 
-
-    switch(a)
-    {
-        case 0:
+    switch(a){
+    
+        case '0':{
+        let name =prompt("Ведите название активности = ");
+        addAttivita(name);
+        break;
+            }
+        case '3':
+            {
+        visualat();
         
         break;
-
-    }
+            }
+        }
 }while(a!=4)
 
 
