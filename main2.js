94a1073aabc8a1cfd06a8a9a1886ef5629cf0f65
danuloshlykow <danilshlykow123@gmail.com>
2025-05-03 12:02:06 +0200

    modifica di menu

--- main2.js
+++ main2.js
@@ -2,4 +2,22 @@ import PromptSync from "prompt-sync";
 
 let prompt = PromptSync();
 
-let a = prompt("Prova");
+let a;
+
+do {
+    console.log("Scegli cosa vuoi fare");
+
+    console.log(" 0=agiungi attivita, 1=cancelazione di attivita , 2=modifica di attivita" );
+    a = prompt("");
+
+
+    switch(a)
+    {
+        case 0:
+        
+        break;
+
+    }
+}while(a!=4)
+
+
